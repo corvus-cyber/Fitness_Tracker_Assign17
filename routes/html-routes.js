35edcafe0f745e93afe7ffa1d8aab5f905f3068c
Corvus <wyatta.hancock@gmail.com>
2020-09-21 10:55:49 -0600

    remembered you need to add a second underscore to get html-routes to function

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,12 +1,16 @@
 const path = require("path");
 const router = require("express").Router();
 
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(_dirname, "../public/exercise.html"))
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
 });
 
 router.get("/statistics", (req, res) =>{
-    res.sendFile(path.join(_dirname, "../public/stats.html"))
-})
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+});
 
 module.exports = router; 
