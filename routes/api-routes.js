00b1601eb0aaa7e0cb17dd22c0b0085d193f6347
Corvus <wyatta.hancock@gmail.com>
2020-09-22 11:38:21 -0600

    established api route for the stats range

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+//Allows user to create a new workout
 router.post("/api/workouts", (req, res) =>{
     Workout.create({})
     .then(workoutDataBase => {
@@ -8,7 +9,7 @@ router.post("/api/workouts", (req, res) =>{
     })
 });
 
-
+//Allows the user to add exercises to their workout
 router.put("/api/workouts/:id", (req, res) =>{
     Workout.findByIdAndUpdate(req.params.id, {$push:{exercises: req.body}}
     )
@@ -17,12 +18,21 @@ router.put("/api/workouts/:id", (req, res) =>{
     })
 })
 
+//Presents stats to the user on the index.html
 router.get("/api/workouts", (req, res) =>{
     Workout.find({})
     .then(workoutDataBase =>{
         res.json(workoutDataBase)
     })
 });
+
+//This establishes the route so that we can see data within the stats.html page
+router.get("/api/workouts/range", (req, res) =>{
+    Workout.find({}).limit(7)
+    .then(workoutDataBase =>{
+        res.json(workoutDataBase)
+    })
+})
 
 
 module.exports = router;
